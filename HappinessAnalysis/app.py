53af5850b86ef6577de7c39f5bcf4aa98c7706ab
RANN1D1901 <navdeeprandhawa0001@gmail.com>
2021-12-25 23:47:35 +1100

    API for uploading the csv file to the database

--- HappinessAnalysis/app.py
+++ HappinessAnalysis/app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import pymongo
 from pymongo import MongoClient
 import pandas as pd
 import json
 
 app = Flask(__name__)
-
+client = MongoClient("localhost", 27017, maxPoolSize = 50)
+DB = client["HappinessReport"]
 @app.route("/")
 def index():
-    return "Hello World!"
+    return "This is Home!"
+
+@app.route("/upload")
+def uploadCSV():
+    filePath = request.args.get('path',type = str)
+    df = pd.read_csv(filePath)
+    data = df.to_dict('recors')
+    
+    collection = DB[filePath]
+    collection.insert_many(data)
+    return("Added Data to database")
+    
+    
