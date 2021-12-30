ea695d91f092abf81127481df8a0b7b5d674d24e
RANN1D1901 <navdeeprandhawa0001@gmail.com>
2021-12-30 18:52:17 +1100

    Dockerized the application

--- HappinessAnalysis/flask-server/app.py
+++ HappinessAnalysis/flask-server/app.py
@@ -6,11 +6,15 @@ import json
 
 app = Flask(__name__)
 client = MongoClient("localhost", 27017, maxPoolSize = 50)
+print(client)
 DB = client["HappinessReport"]
+print(DB)
 @app.route("/")
 def index():
     return "This is Home!"
-
+@app.route("/hello")
+def hello():
+    return "Hello Docker At last working"
 @app.route("/upload")
 def uploadCSV():
     filePath = request.args.get('path',type = str)
@@ -20,5 +24,5 @@ def uploadCSV():
     collection = DB[filePath]
     collection.insert_many(data)
     return("Added Data to database")
-    
-    
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0',port= 3000)
---
+++ HappinessAnalysis/readme.md
@@ -0,0 +1,6 @@
+<h1>HappinessAnalysis:</h1>
+This app builds API endpoints and UI to visualise Happiness Report csv data retrieved from Kaggle.
+
+<h1>To run:</h1>
+<h2>Install Docker for Your OS: https://docs.docker.com/desktop/mac/install/ </h2>
+<h3>Run command: docker compose up</h3>
